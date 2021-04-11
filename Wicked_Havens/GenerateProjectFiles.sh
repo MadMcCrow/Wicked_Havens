180a0af72d5a85869acd3b19201449cf7242e7f7
MadMcCrow <perard.git@outlook.com>
2021-04-11 20:47:47 +0200

    :construction::boom: fix script

--- Wicked_Havens/GenerateProjectFiles.sh
+++ Wicked_Havens/GenerateProjectFiles.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-"../../UnrealEngine/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" $(pwd)/TL.uproject 
+"../../UnrealEngine/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh" $(find $(pwd) -iname "*.uproject")
