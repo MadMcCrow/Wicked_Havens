371bc03784688a2bb8e0c5916fbb7086fc9d8987
MadMcCrow <perard.git@outlook.com>
2022-02-27 16:54:07 +0100

    Revert ":zap::wrench: Added UE5 as a git module"
    This reverts commit 02daa8c594482dc4fa3dc5b4dc59d73ba223e5db.

--- GenerateProjectFiles.py
+++ GenerateProjectFiles.py
@@ -7,18 +7,13 @@ import platform
 import subprocess
 import shlex
 
-game_project_folder  = "Wicked_Havens"
-unreal_engine_folder = os.path.abspath("UnrealEngine")
-project_file = os.path.abspath([a for a in os.listdir(game_project_folder) if ".uproject" in a][0])
-
-args = [ "-game", "-rocket", "-progress", "-engine", "-projectfiles" ]
+project = os.path.abspath([a for a in os.listdir() if ".uproject" in a][0])
+ue_path = os.path.abspath("../UnrealEngine")
 
 if platform.system() == "Linux":
-    cmd = "\"{0}/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh\" {1} {2}".format(str(unreal_engine_folder), " ".join(args), str(project_file))
+    cmd = "\"{0}/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh\" {1} -game -engine".format(str(ue_path),str(project))
 if platform.system() == "Windows":
-    cmd ='\"{0}\\Engine\\Binaries\\DotNET\\UnrealBuildTool\\UnrealBuildTool.exe\" {1} -project=\"{2}\"'.format(str(unreal_engine_folder), " ".join(args), str(project_file))
+    cmd ='\"{0}\\Engine\\Binaries\\DotNET\\UnrealBuildTool\\UnrealBuildTool.exe\" -projectfiles -project=\"{1}\" -game -rocket -progress'.format(str(ue_path),str(project))
 
-print(cmd)
-# Call UAT from command line
 subprocess.call(shlex.split(cmd))
 
